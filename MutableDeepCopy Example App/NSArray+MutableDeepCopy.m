1adc8db1388170ff5beb1ab3004c1c5bf93686aa
Alfonso Maria Tesauro <alfonso.tesauro@gmail.com>
2020-07-06 20:13:22 +0200

    Minor fix.

--- MutableDeepCopy Example App/NSArray+MutableDeepCopy.m
+++ MutableDeepCopy Example App/NSArray+MutableDeepCopy.m
@@ -33,32 +33,4 @@
     
 }
 
-+ (NSMutableDictionary *)doDeepMutateDictionary:(NSDictionary *)dict
-{
-
-    NSMutableDictionary *toReturn = [NSMutableDictionary dictionaryWithDictionary:dict];
-    
-    [dict enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
-        
-        
-        if ([obj isKindOfClass:[NSDictionary class]])
-        {
-            
-            NSMutableDictionary *testg = [self doDeepMutateDictionary:obj];
-            [toReturn setValue:testg forKey:key];
-        }
-        else
-            if ([obj isKindOfClass:[NSArray class]])
-            {
-                NSMutableArray *theNew = [self doDeepMutateArray:obj];
-                
-                [toReturn setValue:theNew forKey:key];
-                
-            }
-        
-    }];
-    
-    return toReturn;
-}
-
 @end
